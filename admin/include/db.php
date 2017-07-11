1418985e52c4bc49ef9b14f0809e2ae10d955a28
Dhani <dhani.534@gmail.com>
2017-07-11 09:36:04 +0700

    Config db connetion

--- admin/include/db.php
+++ admin/include/db.php
@@ -5,7 +5,9 @@
  * Date: 01/07/2017
  * Time: 8:00
  */
-$dbc = mysqli_connect("localhost", "root", "", "CMS", "3306");
+$handle = fopen('config.txt',"r");
+$db = fgetcsv($handle,'0',',');
+$dbc = mysqli_connect("$data[0]", "$data[1]", "$data[2]", "$data[3]");
 if (!$dbc) {
     die($dbc);
 }
